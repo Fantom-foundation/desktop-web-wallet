b1c27cbc6f45deaffb570bb173080d9e9620b14e
razat <rverma@innow8apps.com>
2018-11-26 18:42:09 +0530

    account form ui design

--- src/view/components/forms/create-account/index.js
+++ src/view/components/forms/create-account/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Row, Col, Form, FormGroup, Input } from 'reactstrap';
 
+import cross from './cross.svg';
+import check from './check.svg';
+import user from './user.svg';
+import lock from './lock.svg';
+
 export default class CreatAccount extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -11,29 +16,56 @@ export default class CreatAccount extends React.PureComponent {
     return (
       <Form id="create-account-form">
         <FormGroup>
-          <Input type="text" name="name" placeholder="Enter Name" />
+          <Input
+            type="text"
+            name="name"
+            placeholder="Enter Name"
+            style={{ backgroundImage: `url(${user})` }}
+          />
         </FormGroup>
         <Row>
           <Col>
             <FormGroup>
-              <Input type="password" name="pass" placeholder="Enter Pass" />
+              <Input
+                type="password"
+                name="pass"
+                placeholder="Enter Pass"
+                style={{ backgroundImage: `url(${lock})` }}
+              />
             </FormGroup>
             <FormGroup>
-              <Input type="password" name="name" placeholder="Re Enter Pass" />
+              <Input
+                type="password"
+                name="name"
+                placeholder="Re Enter Pass"
+                style={{ backgroundImage: `url(${lock})` }}
+              />
             </FormGroup>
           </Col>
-          <Col sm={3}>
-            <div>
-              <ul>
-                <li className="correct">8+ Characters</li>
-                <li className="false">1+ Capilital Letter</li>
-                <li className="false">1+ Number</li>
-              </ul>
-            </div>
+          <Col md={4} lg={3}>
+            <ul className="pass-validator">
+              <li className="correct">
+                <img src={check} alt="correct" className="ico" />
+                8+ Characters
+              </li>
+              <li className="false">
+                <img src={cross} alt="invalid" className="ico" />
+                1+ Capilital Letter
+              </li>
+              <li className="false">
+                <img src={cross} alt="invalid" className="ico" />
+                1+ Number
+              </li>
+            </ul>
           </Col>
         </Row>
         <FormGroup>
-          <Input type="text" name="name" placeholder="Password Hint" />
+          <Input
+            type="text"
+            name="name"
+            placeholder="Password Hint"
+            style={{ backgroundImage: `url(${lock})` }}
+          />
         </FormGroup>
       </Form>
     );
