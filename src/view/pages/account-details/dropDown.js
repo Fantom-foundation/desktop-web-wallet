447c39f30302ea036af8e7498c235b324dbc15a4
Razat <rverma@innow8apps.com>
2018-12-03 20:01:57 +0530

    account-details ui html

---
+++ src/view/pages/account-details/dropDown.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+export default class DropDown extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false,
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen,
+    }));
+  }
+
+  render() {
+    return (
+      <Dropdown
+      // isOpen={this.state.dropdownOpen} toggle={this.toggle}
+      >
+        <DropdownToggle caret>Dropdown</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    );
+  }
+}
