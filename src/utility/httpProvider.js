fbdf30824b3305e21c699bec047e5a38c0ccbfe4
Neeraj <neeraj.17011996@gmail.com>
2018-12-08 19:45:15 +0530

    Started integration of GraphQL

---
+++ src/utility/httpProvider.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+class HttpDataProvider {
+  constructor() {
+    if (HttpDataProvider.instance) HttpDataProvider.instance = this;
+
+    // axios Configuration
+    axios.defaults.headers.common = {
+      'Content-Type': 'application/json',
+    };
+    axios.interceptors.response.use(
+      response => {
+        console.log(response, 'response');
+        return response;
+      },
+      error =>
+        // if (error.response.status === 401) {
+        //   sessionStorage.clear();
+        //   window.location.href = '/login';
+        // }
+        error
+    );
+
+    return HttpDataProvider.instance;
+  }
+
+  get = url => axios.get(url);
+
+  post = (url, data) => axios.post(url, data);
+
+  setHeaderToken = token => {
+    axios.defaults.headers.common = { 'x-auth-token': token };
+    sessionStorage.setItem('x-auth-token', token);
+  };
+
+  removeHeaderToken = () => {
+    axios.defaults.headers = { 'x-auth-token': '' };
+  };
+}
+
+const httpProvider = new HttpDataProvider();
+
+Object.freeze(httpProvider);
+
+export default httpProvider;
