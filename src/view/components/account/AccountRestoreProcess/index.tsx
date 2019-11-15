a4be9623a3910c8a80e9dd1f09f6a6d6c64ad95a
devintegral6 <integral6@sfxdx.ru>
2019-11-15 10:39:56 +0700

    fixed restore icons for electron

--- src/view/components/account/AccountRestoreProcess/index.tsx
+++ src/view/components/account/AccountRestoreProcess/index.tsx
@@ -3,10 +3,11 @@ import { PanelProgress } from '~/view/components/panels/PanelProgress';
 
 import create_account from '~/images/icons/create_account.svg';
 import account_confirm from '~/images/icons/account_confirm.svg';
+import { getURL } from '~/utility/dom';
 
 const STAGES = [
-  { icon: create_account, title: "Create Account" },
-  { icon: account_confirm, title: "Confirm" },
+  { icon: getURL(create_account), title: "Create Account" },
+  { icon: getURL(account_confirm), title: "Confirm" },
 ];
 
 interface IProps {
