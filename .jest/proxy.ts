6cd58f8385c5f8f00bdf50d9af2f6c15448e1186
devintegral6 <integral6@sfxdx.ru>
2019-11-26 15:47:27 +0700

    cleanup

--- .jest/proxy.ts
+++ .jest/proxy.ts
@@ -2,7 +2,6 @@ module.exports = new Proxy(
   {},
   {
     get: function getter(target, key) {
-      console.log("HELLO!!!");
       if (key === '__esModule') {
         // True instead of false to pretend we're an ES module.
         return true;
