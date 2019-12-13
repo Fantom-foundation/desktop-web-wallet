12bbbbf9245bf37530c522d6190c20688dbe0c47
Kapil Bindal <kapilbindal1@gmail.com>
2019-12-13 11:12:36 +0530

    Ui Updates

--- src/view/components/layout/DashboardLayout/index.tsx
+++ src/view/components/layout/DashboardLayout/index.tsx
@@ -9,27 +9,29 @@ export default ({ children }) => (
         <Sidebar />
       </div>
       <div className={styles.contentWrapper}>
-        <main>
-          <div className="d-flex justify-content-end mb-3">
-            <p className={styles.sync}>
-              <CheckIcon />
-              Synchronized
-            </p>
-          </div>
-          <div>
-            <p className={styles.label}>Address</p>
-          </div>
-          <div className={styles.hashWrapper}>
-            <p className={styles.hash}>
-              0xD94Cfae08391a7dBbCCF6B98c5115Be854c09006
-            </p>
-            <div className={styles.hashHandlers}>
-              <button>
-                <CopyIcon />
-              </button>
-              <button>
-                <QrIcon />
-              </button>
+        <main className={styles.main}>
+          <div className={styles.contentHeader}>
+            <div className="d-flex justify-content-end mb-3">
+              <p className={styles.sync}>
+                <CheckIcon />
+                Synchronized
+              </p>
+            </div>
+            <div>
+              <p className={styles.label}>Address</p>
+            </div>
+            <div className={styles.hashWrapper}>
+              <p className={styles.hash}>
+                0xD94Cfae08391a7dBbCCF6B98c5115Be854c09006
+              </p>
+              <div className={styles.hashHandlers}>
+                <button>
+                  <CopyIcon />
+                </button>
+                <button>
+                  <QrIcon />
+                </button>
+              </div>
             </div>
           </div>
           {children}
