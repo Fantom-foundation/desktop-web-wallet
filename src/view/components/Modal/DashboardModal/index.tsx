bd0d318d86003edd7dd05def8177573a754ea118
Kapil Bindal <kapilbindal1@gmail.com>
2019-12-18 12:53:55 +0530

    Dashboard Modal Updates

---
+++ src/view/components/Modal/DashboardModal/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import classnames from 'classnames';
+export default ({ isOpen, toggle, children, title, bodyClassName = '' }) => (
+  <Modal
+    isOpen={isOpen}
+    toggle={toggle}
+    className={classnames('modal-dialog-centered', styles.modal)}
+  >
+    <ModalHeader className={styles.header} toggle={toggle}>
+      {title}
+    </ModalHeader>
+    <ModalBody className={classnames(styles.body, bodyClassName)}>
+      {children}
+    </ModalBody>
+  </Modal>
+);
