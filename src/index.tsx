ad6b62b0d95a7e3b577a45cbe429f3caa32d4368
Kapil Bindal <kapilbindal1@gmail.com>
2020-01-23 17:18:28 +0530

    translations locales

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom';
 import App from '~/App';
 import './main.scss';
 import * as serviceWorker from '~/serviceWorker';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import { englishTranslation } from './utility/translations/english';
+import { koreanTranslation } from './utility/translations/korean';
+import { chineseTranslation } from './utility/translations/chinese';
+
+
+
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: {
+        translation: englishTranslation,
+      },
+      kor: {
+        translation: koreanTranslation,
+      },
+      chi: {
+        translation: chineseTranslation,
+      },
+    },
+    lng: "en",
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
