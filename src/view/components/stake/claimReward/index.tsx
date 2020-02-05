4b28143cca2526cab25bd99314f788e184e82c00
Kapil Bindal <kapilbindal1@gmail.com>
2020-02-05 14:48:44 +0530

    claim ui updates

--- src/view/components/stake/claimReward/index.tsx
+++ src/view/components/stake/claimReward/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, Button, Modal, ModalBody } from 'reactstrap';
 import classnames from 'classnames';
 import styles from './styles.module.scss';
+import { Input } from '../../forms';
 
 const InfoModal = () => {
   return (
@@ -11,7 +12,12 @@ const InfoModal = () => {
       </button>
       <Modal isOpen={false} centered className={styles.infoModal}>
         <ModalBody className={styles.modalBody}>
-          <h2 className="text-center mb-4 pb-2">Claim your rewards</h2>
+          <h2 className="text-center mb-4 pb-2">
+            Claim your rewards{' '}
+            <span className={styles.cross}>
+              <i className="fas fa-times"></i>
+            </span>
+          </h2>
           <h4 className="mb-3">You can claim your rewards in FTM or fUSD.</h4>
           <h4 className="mb-3">
             FTM rewards will be available after six months or when 80% of the
@@ -43,31 +49,44 @@ const InfoModal = () => {
 const ClaimfUSDModal = () => {
   return (
     <>
-      <Modal isOpen={false} centered className={styles.infoModal}>
+      <Modal isOpen={false} centered className={styles.claimUsdModal}>
         <ModalBody className={styles.modalBody}>
-          <h2 className="text-center mb-4 pb-2">Claim your rewards</h2>
-          <h4 className="mb-3">You can claim your rewards in FTM or fUSD.</h4>
-          <h4 className="mb-3">
-            FTM rewards will be available after six months or when 80% of the
-            circulating supply is staked, whichever comes first.
-          </h4>
-          <h4 className="mb-3">
-            fUSD rewards are available at any time.
-            <br />
-            fUSD is a stablecoin pegged to the dollar: 1 fUSD = $1.
-          </h4>
-          <h3 className="mb-2">What can I do with fUSD?</h3>
-          <h4>
-            You can use fUSD in the DeFi section of your wallet. With it you
-            can:
-          </h4>
-          <ul className="pl-4">
-            <li>buy and sell synthetic tokens.</li>
-            <li>use it as a collateral to borrow other assets.</li>
-            <li>
-              use it as a liquidity provider for loans and earn interest on it.
-            </li>
-          </ul>
+          <h2 className="text-center mb-4 pb-2">
+            Claim fUSD
+            <span className={styles.cross}>
+              <i className="fas fa-times"></i>
+            </span>
+          </h2>
+          <div className={styles.balanceCard}>
+            <div className="mb-3 d-flex justify-content-between">
+              <h3 className="m-0 opacity-5">Available to claim</h3>
+              <p className="mb-0">1,345 FTM</p>
+            </div>
+            <div className="mb-3 d-flex justify-content-between">
+              <h3 className="m-0 opacity-5">FTM price</h3>
+              <p className="mb-0">$ 20,966,562.71</p>
+            </div>
+          </div>
+          <div className={classnames(styles.receiveRewards, 'my-5')}>
+            <h2>You will receive</h2>
+            <h1>13.92075 fUSD</h1>
+            <p>
+              The claimed rewards will be immediately available in your wallet.
+            </p>
+          </div>
+          <div className={styles.passwordInput}>
+            <Input
+              type="password"
+              label="Password"
+              value=""
+              handler={() => {}}
+              isError=""
+              errorMsg=""
+            />
+          </div>
+          <div className="text-center">
+            <Button color="primary">Claim now</Button>
+          </div>
         </ModalBody>
       </Modal>
     </>
