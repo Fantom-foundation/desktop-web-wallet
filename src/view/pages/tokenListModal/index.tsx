b7c10052a167a922f1c31c1464d23a72ab8f4d9a
Kapil Bindal <kapilbindal1@gmail.com>
2020-02-10 16:44:44 +0530

    minor fixes

--- src/view/pages/tokenListModal/index.tsx
+++ src/view/pages/tokenListModal/index.tsx
@@ -21,39 +21,41 @@ export default ({ isOpen = false, toggle }) => {
           </div>
           <Table className={styles.table}>
             <thead className={styles.tableHead}>
-              <th
-                className={classnames({
-                  [styles.up]: false,
-                  [styles.down]: false,
-                })}
-              >
-                <div className={styles.tableHeading}>
-                  Asset
-                  <ArrowUpDownIcon />
-                </div>
-              </th>
-              <th
-                className={classnames({
-                  [styles.up]: false,
-                  [styles.down]: true,
-                })}
-              >
-                <div className={styles.tableHeading}>
-                  Borrow APR
-                  <ArrowUpDownIcon />
-                </div>
-              </th>
-              <th
-                className={classnames({
-                  [styles.up]: false,
-                  [styles.down]: false,
-                })}
-              >
-                <div className={styles.tableHeading}>
-                  Price (fUSD)
-                  <ArrowUpDownIcon />
-                </div>
-              </th>
+              <tr>
+                <th
+                  className={classnames({
+                    [styles.up]: false,
+                    [styles.down]: false,
+                  })}
+                >
+                  <div className={styles.tableHeading}>
+                    Asset
+                    <ArrowUpDownIcon />
+                  </div>
+                </th>
+                <th
+                  className={classnames({
+                    [styles.up]: false,
+                    [styles.down]: true,
+                  })}
+                >
+                  <div className={styles.tableHeading}>
+                    Borrow APR
+                    <ArrowUpDownIcon />
+                  </div>
+                </th>
+                <th
+                  className={classnames({
+                    [styles.up]: false,
+                    [styles.down]: false,
+                  })}
+                >
+                  <div className={styles.tableHeading}>
+                    Price (fUSD)
+                    <ArrowUpDownIcon />
+                  </div>
+                </th>
+              </tr>
             </thead>
             <tbody>
               <SearchTokenList />
